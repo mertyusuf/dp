3289a8a889c8a9034cb9025f970ca0f1bb6b95c2
Mert yusuf Ünveren <123647731+mertyusuf@users.noreply.github.com>
2024-09-26 00:59:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('Machine Learning App')
 
 st.info('This is a app builds a machine learning model!')
 with st.expender('Data'):
-st.write('**Raw data**')
+ st.write('**Raw data**')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
 df
