96510fcb2e3462555b500901541df7d07e0e7c93
Mert yusuf Ünveren <123647731+mertyusuf@users.noreply.github.com>
2024-09-26 00:51:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import pandas as pd
 st.title('Machine Learning App')
 
 st.info('This is a app builds a machine learning model!')
-df = pd.read_csv('https://raw.githubusercontent.com/mertyusuf/dp/master/credit22.csv', sep=',')
+df = pd.read_csv('https://raw.githubusercontent.com/mertyusuf/dp/master/credit22.csv', sep=';', encoding='utf-8')
 df
